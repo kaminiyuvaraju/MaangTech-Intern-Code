713fa195a234635e55c0a8ec9f49ee2165934f1a
Kamini Yuvaraju <146220949+kaminiyuvaraju@users.noreply.github.com>
2024-01-20 17:22:22 +0530

    Add files via upload

---
+++ Javascript/20-01-2024/BOM.js
@@ -0,0 +1,18 @@
+document.getElementById("demo1").innerHTML =
+"Browser inner window width: " + window.innerWidth + "px<br>" +
+"Browser inner window height: " + window.innerHeight + "px";
+
+document.getElementById("demo2").innerHTML = 
+"Screen width is " + screen.width;
+
+document.getElementById("demo3").innerHTML = 
+"Screen height is " + screen.height;
+
+document.getElementById("demo4").innerHTML =
+"Available Screen Height: " + screen.availHeight;
+
+document.getElementById("demo5").innerHTML =
+"Screen Color Depth: " + screen.colorDepth;
+
+document.getElementById("demo6").innerHTML =
+"Screen Pixel Depth: " + screen.pixelDepth;
---
+++ Javascript/20-01-2024/WindowLocation.js
@@ -0,0 +1,14 @@
+document.getElementById("demo1").innerHTML = 
+"The full URL of this page is:<br>" + window.location.href;
+
+document.getElementById("demo2").innerHTML =
+"Page hostname is " + window.location.hostname;
+
+document.getElementById("demo3").innerHTML =
+"Page path is: " + window.location.pathname;
+
+document.getElementById("demo4").innerHTML =
+"Page protocol is " + window.location.protocol;
+
+document.getElementById("demo5").innerHTML =
+"Port number is " + window.location.port;
