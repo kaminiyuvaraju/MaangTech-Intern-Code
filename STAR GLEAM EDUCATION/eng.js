7c8af13d752537f049b50fe9c7a95de594e117dd
Kamini Yuvaraju <146220949+kaminiyuvaraju@users.noreply.github.com>
2024-04-13 18:47:16 +0530

    Add files via upload

---
+++ STAR GLEAM EDUCATION/eng.js
@@ -0,0 +1,11 @@
+<script>
+    // Function to toggle the visibility of English practice section
+    function toggleEnglishPractice() {
+           var  englishPracticeSection = document.getElementById("englishPractice");
+        if (englishPracticeSection.style.display === "none") {
+            englishPracticeSection.style.display = "block";
+        } else {
+            englishPracticeSection.style.display = "none";
+        }
+    }
+</script>
